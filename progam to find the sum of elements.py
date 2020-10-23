d1b0dfa1a3c0f40e245dda22109c4e603885e178
sruthiviswanathan-28 <70143945+sruthiviswanathan-28@users.noreply.github.com>
2020-10-24 00:22:30 +0530

    Create progam to find the sum of elements.py

---
+++ progam to find the sum of elements.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 22:38:48 2020
+
+@author: sruth
+"""
+
+def sum_list(items):
+    sum=0
+    for x in items:
+        sum+=x
+    return sum
+print (sum_list([1,2,3,4,5]))    
