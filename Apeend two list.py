b305a3f49732cc467ba061ff6f661a0e657f7736
sruthiviswanathan-28 <70143945+sruthiviswanathan-28@users.noreply.github.com>
2020-10-24 00:22:09 +0530

    Create Apeend two list.py

---
+++ Apeend two list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 23:26:58 2020
+
+@author: sruth
+"""
+
+list1 = [1, 2, 3, 0]
+list2 = ['Red', 'Green', 'Black']
+final_list = list1 + list2
+print(final_list)
